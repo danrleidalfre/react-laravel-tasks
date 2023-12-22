f1384d2bd57842fae0edf290a6898e53d25f02fb
Danrlei <danrleidalfre@gmail.com>
2023-12-22 16:44:25 -0300

    rename resource fields tags

--- api/app/Http/Resources/TagResource.php
+++ api/app/Http/Resources/TagResource.php
@@ -10,8 +10,8 @@ class TagResource extends JsonResource
     public function toArray(Request $request): array
     {
         return [
-            'id' => $this->id,
-            'title' => $this->title,
+            'value' => $this->id,
+            'label' => $this->title,
             'created_at' => $this->created_at->format('d/m/Y H:i'),
         ];
     }
